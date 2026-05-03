6cefb0d9b0496df1c56fd8e137d2e880d7448223
mko1989 <44507953+mko1989@users.noreply.github.com>
2026-05-03 15:07:58 +0200

    Delete CasparCG_Enhanced-main directory

--- CasparCG_Enhanced-main/src/common/os/thread.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <string>
-
-namespace caspar {
-
-void set_thread_name(const std::wstring& name);
-void set_thread_realtime_priority();
-} // namespace caspar
--- CasparCG_Enhanced-main/tools/verify-format.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# override args to diff changes
-export CLANG_ARGS=-output-replacements-xml
-
-SCRIPT_ROOT=$(dirname "${0}")/..
-"${SCRIPT_ROOT}/tools/update-format.sh"
